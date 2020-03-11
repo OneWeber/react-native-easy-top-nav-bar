6245e5f89a1b2501391238d8cf1f13fb20487404
zky1994 <229763015@qq.com>
2020-03-11 13:37:42 +0800

    update example

---
+++ example/App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import RNEasyTopNavBar from 'react-native-easy-top-nav-bar';
+
+export default class App extends Component{
+  getLeftButton() {
+    return <View>
+      <Text style={{color: '#fff'}}>返回</Text>
+    </View>
+  }
+  getRightButton() {
+    return <View>
+      <Text style={{color: '#fff'}}>筛选</Text>
+    </View>
+  }
+  render(){
+    return(
+        <RNEasyTopNavBar
+            title={'标题'}
+            backgroundTheme={'#008489'}
+            leftButton={this.getLeftButton()}
+            rightButton={this.getRightButton()}
+        />
+    )
+  }
+}
+const styles = StyleSheet.create({
+
+})
