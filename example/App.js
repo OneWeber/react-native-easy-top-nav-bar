cf2e0f7b434515f28ce5a6622ec44a3acae651ec
zky1994 <229763015@qq.com>
2020-03-12 12:06:30 +0800

    update

--- example/App.js
+++ example/App.js
@@ -28,11 +28,23 @@ export default class App extends Component{
       <Text style={{color: '#fff'}}>more</Text>
     </View>
   }
+  getTitleDialog(){
+    return <View>
+      <Text>这里是自定义内容区域</Text>
+    </View>
+  }
+  getTitleView() {
+    return <View style={{flexDirection: 'row',justifyContent:'center',alignItems:'center'}}>
+      <Text style={{color: '#fff', fontSize:16,fontWeight: 'bold'}}>筛选</Text>
+    </View>
+  }
   render(){
     return(
         <RNEasyTopNavBar
             title={'this is title'}
             backgroundTheme={'#008489'}
+            titleDialog={this.getTitleDialog()}
+            titleView={this.getTitleView()}
             leftButton={this.getLeftButton()}
             rightButton={this.getRightButton()}
             ellipsizeModeType={'tail'}
